95c16616d2289465dc6f5f855d00ec32daf4e5d0
pengkm <286690745@qq.com>
2022-10-21 10:31:18 +0800

    feat：player control

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject cameraCenterPoint;
-    private float rotateSpeed = 45.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,14 +21,7 @@ public class GameManager : MonoBehaviour
     // use Q and E key to rotate camera
     void RotateCamera()
     {
-        if (Input.GetKey("left"))
-        {
-            cameraCenterPoint.gameObject.transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey("right"))
-        {
-            cameraCenterPoint.gameObject.transform.Rotate(Vector3.up * -rotateSpeed * Time.deltaTime);
-        }
+        
     }
 
 
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    private float forwardSpeed = 10.0f;
+    private float rotateSpeed = 45.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     
+        RotationPlayer();
+        MovePlayer();   
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    //  use s/w key to let user moved
+    void MovePlayer()
+    {
+        if (Input.GetKey("s"))
+        {
+            gameObject.transform.Translate(Vector3.back * forwardSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            gameObject.transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+        }
+    }
+
+    //  use a/d key to let user moved
+    void RotationPlayer()
+    {
+        if (Input.GetKey("a"))
+        {
+            gameObject.transform.Rotate(Vector3.down, rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            gameObject.transform.Rotate(Vector3.up , rotateSpeed * Time.deltaTime);
+        }
     }
 }
